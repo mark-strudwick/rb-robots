776602ee323e1be270d00c4e0c85664971aa0d04
Mark Strudwick <markstrudwick@outlook.com>
2026-05-28 22:43:21 +0100

    Add methods to the grid to work with scents and check if a position is in the bounds of the grid

--- src/models/grid.ts
+++ src/models/grid.ts
@@ -1,11 +1,31 @@
 import { type RobotOrientation } from "./robot.js";
 
 export type GridPosition = { x: number; y: number };
+export type ScentPosition = { position: GridPosition; orientation: RobotOrientation };
 
 export class Grid {
-  private scents: { position: GridPosition; orientation: RobotOrientation }[];
+  private scents: ScentPosition[] = [];
 
-  constructor(private readonly boundaryPosition: { x: number; y: number }) {
-    this.scents = [];
+  constructor(private readonly boundaryPosition: { x: number; y: number }) {}
+
+  public GetScents(): ScentPosition[] {
+    return this.scents;
+  }
+
+  public AddScent(scent: ScentPosition): void {
+    this.scents.push(scent);
+  }
+
+  public CheckValidPosition(position: GridPosition): boolean {
+    if (
+      position.x < 0 ||
+      position.x > this.boundaryPosition.x ||
+      position.y < 0 ||
+      position.y > this.boundaryPosition.y
+    ) {
+      return false;
+    }
+
+    return true;
   }
 }
