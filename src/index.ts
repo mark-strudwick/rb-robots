bd98eadb3e94000f7dc91ad4def42a68be229532
Mark Strudwick <markstrudwick@outlook.com>
2026-05-28 20:33:03 +0100

    Parse a hardcoded file and split it into lines

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
+import { readFileSync } from "fs";
+
 function main() {
   console.info("Starting RB Robots");
+
+  const filePath = "input-instructions.txt";
+  const fileContents = readFileSync(filePath, "utf8").toString();
+  console.info(fileContents);
+
+  const lines: string[] = fileContents.split(/\r?\n/).filter((line) => line.trim() !== "");
+  console.info(lines);
+
+  // first line
+  // x y
+  // 2 lines per robot (seperated by an empty line)
+  // initial coordinate and orientation
+  // x y ori
+  // Instructions
+  // RFRFRFRF
 }
 
 main();
