60facb5222623f717392413d4e2ce324a7696f81
Mark Strudwick <markstrudwick@outlook.com>
2026-05-28 22:43:44 +0100

    Add methods to the robot to process instructions

--- src/models/robot.ts
+++ src/models/robot.ts
@@ -1,17 +1,122 @@
-import { type GridPosition } from "./grid.js";
+import { Grid, type GridPosition, type ScentPosition } from "./grid.js";
 
 export type RobotOrientation = "N" | "E" | "S" | "W";
 export type RobotInstruction = "L" | "R" | "F";
 export type RobotStatus = "OK" | "LOST";
 
 export class Robot {
-  private status: RobotStatus;
+  private status: RobotStatus = "OK";
 
   constructor(
-    private readonly position: GridPosition,
-    private readonly orientation: RobotOrientation,
-    private readonly instructions: string
-  ) {
-    this.status = "OK";
+    private position: GridPosition,
+    private orientation: RobotOrientation,
+    private readonly instructions: RobotInstruction[]
+  ) {}
+
+  public GetStatus(): RobotStatus {
+    return this.status;
+  }
+
+  public GetPosition(): GridPosition {
+    return this.position;
+  }
+
+  public GetOrientation(): RobotOrientation {
+    return this.orientation;
+  }
+
+  public ProcessInstructions(grid: Grid): void {
+    this.instructions.forEach((instruction) => {
+      if (this.status === "LOST") {
+        return;
+      }
+
+      switch (instruction) {
+        case "L":
+        case "R":
+          this.rotate(instruction);
+          break;
+        case "F":
+          this.moveForward(instruction, grid);
+          break;
+        default:
+          throw new Error("Invalid instruction");
+      }
+
+      // if it's F then handle forward
+      // check if there's a scent where it wants to go
+    });
+  }
+
+  private rotate(instruction: Exclude<RobotInstruction, "F">): void {
+    switch (instruction) {
+      case "L":
+        if (this.orientation === "N") {
+          this.orientation = "W";
+        }
+        if (this.orientation === "E") {
+          this.orientation = "N";
+        }
+        if (this.orientation === "S") {
+          this.orientation = "E";
+        }
+        if (this.orientation === "W") {
+          this.orientation = "S";
+        }
+        break;
+      case "R":
+        if (this.orientation === "N") {
+          this.orientation = "E";
+        }
+        if (this.orientation === "E") {
+          this.orientation = "S";
+        }
+        if (this.orientation === "S") {
+          this.orientation = "W";
+        }
+        if (this.orientation === "W") {
+          this.orientation = "N";
+        }
+        break;
+      default:
+        throw new Error("Invalid rotation instruction");
+    }
+  }
+
+  private moveForward(instruction: Exclude<RobotInstruction, "L" | "R">, grid: Grid): void {
+    if (
+      grid
+        .GetScents()
+        .some(
+          (s) =>
+            s.position.x === this.position.x && s.position.y === this.position.y && s.orientation === this.orientation
+        )
+    ) {
+      return;
+    }
+
+    const nextPosition = this.getNextPosition();
+    const validNextPosition = grid.CheckValidPosition(nextPosition);
+
+    if (!validNextPosition) {
+      this.status = "LOST";
+    }
+
+    this.position = nextPosition;
+  }
+
+  private getNextPosition(): GridPosition {
+    switch (this.orientation) {
+      case "N":
+        return { x: this.position.x, y: (this.position.y += 1) };
+      case "E":
+        return { x: (this.position.x += 1), y: this.position.y };
+      case "S":
+        return { x: this.position.x, y: this.position.y - 1 };
+      case "W":
+        return { x: (this.position.x -= 1), y: this.position.y };
+      default:
+        throw new Error("Invalid");
+    }
   }
 }
