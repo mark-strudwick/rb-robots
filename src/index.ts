2425f4c90e61f6e9d064cd99f24b6b968e8e6e05
Mark Strudwick <markstrudwick@outlook.com>
2026-05-28 22:44:18 +0100

    Add logic to process the grid and run robots

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 
 import { Grid } from "./models/grid.js";
-import { Robot, type RobotOrientation } from "./models/robot.js";
+import { Robot, type RobotInstruction, type RobotOrientation } from "./models/robot.js";
 
 function main() {
   console.info("Starting RB Robots");
@@ -13,20 +13,24 @@ function main() {
   const lines: string[] = fileContents.split(/\r?\n/).filter((line) => line.trim() !== "");
   console.info(lines);
 
-  const parsedInput = parseInputLines(lines);
-  console.info(parsedInput);
+  const { grid, robots } = parseInputLines(lines);
+  console.info(grid);
+  console.info(robots);
 
-  // first line
-  // x y
-  // 2 lines per robot (seperated by an empty line)
-  // initial coordinate and orientation
-  // x y ori
-  // Instructions
-  // RFRFRFRF
+  robots.forEach((robot) => {
+    robot.ProcessInstructions(grid);
+    if (robot.GetStatus() === "LOST") {
+      grid.AddScent({ position: robot.GetPosition(), orientation: robot.GetOrientation() });
+    }
+  });
+
+  console.log("Processed all robot instructions");
+  console.log(robots);
+  // TODO: produce report
 }
 
 function parseInputLines(input: string[]): { grid: Grid; robots: Robot[] } {
-  // validate inputs
+  // TODO: Validate inputs?
 
   const boundingPositionLine = input[0];
   if (!boundingPositionLine) {
@@ -49,12 +53,15 @@ function parseInputLines(input: string[]): { grid: Grid; robots: Robot[] } {
       throw new Error();
     }
 
+    const instructions = instructionsLine.split("") as RobotInstruction[];
+
     const [x, y, orientation] = positionLine.split(" ");
     if (!x || !y || !orientation) {
       throw new Error();
     }
 
-    robots.push(new Robot({ x: Number(x), y: Number(y) }, orientation as RobotOrientation, instructionsLine));
+    // TODO: Check if starting position is a valid position in the grid
+    robots.push(new Robot({ x: Number(x), y: Number(y) }, orientation as RobotOrientation, instructions));
   }
 
   return { grid, robots };
